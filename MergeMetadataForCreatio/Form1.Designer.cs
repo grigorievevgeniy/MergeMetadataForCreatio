0e71a5a41c70dcdc4d2029f6dd555c56a9d4b255
Grigoriev <timeinvest27@gmail.com>
2021-05-30 13:53:09 +0300

    Реализован основной функционал.

--- MergeMetadataForCreatio/Form1.Designer.cs
+++ MergeMetadataForCreatio/Form1.Designer.cs
@@ -29,33 +29,79 @@ namespace MergeMetadataForCreatio
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbValue1 = new System.Windows.Forms.TextBox();
+            this.tbValue2 = new System.Windows.Forms.TextBox();
+            this.lblFirstValue = new System.Windows.Forms.Label();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.lblSecondValue = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbValue1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(600, 400);
-            this.textBox1.TabIndex = 0;
+            this.tbValue1.Location = new System.Drawing.Point(13, 24);
+            this.tbValue1.Multiline = true;
+            this.tbValue1.Name = "tbValue1";
+            this.tbValue1.Size = new System.Drawing.Size(600, 213);
+            this.tbValue1.TabIndex = 0;
             // 
-            // textBox2
+            // tbValue2
             // 
-            this.textBox2.Location = new System.Drawing.Point(619, 13);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(600, 400);
-            this.textBox2.TabIndex = 1;
+            this.tbValue2.Location = new System.Drawing.Point(619, 24);
+            this.tbValue2.Multiline = true;
+            this.tbValue2.Name = "tbValue2";
+            this.tbValue2.Size = new System.Drawing.Size(600, 213);
+            this.tbValue2.TabIndex = 1;
+            // 
+            // lblFirstValue
+            // 
+            this.lblFirstValue.AutoSize = true;
+            this.lblFirstValue.Location = new System.Drawing.Point(12, 9);
+            this.lblFirstValue.Name = "lblFirstValue";
+            this.lblFirstValue.Size = new System.Drawing.Size(56, 13);
+            this.lblFirstValue.TabIndex = 2;
+            this.lblFirstValue.Text = "First Value";
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Font = new System.Drawing.Font("Cooper Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMerge.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnMerge.Location = new System.Drawing.Point(576, 243);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(75, 23);
+            this.btnMerge.TabIndex = 3;
+            this.btnMerge.Text = "MERGE";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // lblSecondValue
+            // 
+            this.lblSecondValue.AutoSize = true;
+            this.lblSecondValue.Location = new System.Drawing.Point(616, 8);
+            this.lblSecondValue.Name = "lblSecondValue";
+            this.lblSecondValue.Size = new System.Drawing.Size(74, 13);
+            this.lblSecondValue.TabIndex = 4;
+            this.lblSecondValue.Text = "Second Value";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(12, 272);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(1206, 124);
+            this.tbResult.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1230, 556);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1230, 495);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.lblSecondValue);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.lblFirstValue);
+            this.Controls.Add(this.tbValue2);
+            this.Controls.Add(this.tbValue1);
             this.Name = "Form1";
             this.Text = "Merge Metadata From Creatio";
             this.ResumeLayout(false);
@@ -65,8 +111,12 @@ namespace MergeMetadataForCreatio
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbValue1;
+        private System.Windows.Forms.TextBox tbValue2;
+        private System.Windows.Forms.Label lblFirstValue;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Label lblSecondValue;
+        private System.Windows.Forms.TextBox tbResult;
     }
 }
 
--- MergeMetadataForCreatio/Form1.cs
+++ MergeMetadataForCreatio/Form1.cs
@@ -1,20 +1,80 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MergeMetadataForCreatio
 {
     public partial class Form1 : Form
     {
+        string prefix;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            prefix = "Укажите здесь значение префикса!!!";
+
+            string value1 = tbValue1.Text;
+            var guids1 = GetGuids(value1);
+            lblFirstValue.Text += " " + guids1.Count + " значений.";
+
+            string value2 = tbValue2.Text;
+            var guids2 = GetGuids(value2);
+            lblSecondValue.Text += " " + guids2.Count + " значений.";
+
+            var finishGuids = GetTotalList(guids1, guids2);
+
+            tbResult.Text = ConvertGuidsToString(finishGuids);
+        }
+
+        #region Help Methods
+
+        string ConvertGuidsToString(List<Guid> guids) => prefix + JsonConvert.SerializeObject(guids);
+
+        List<Guid> GetTotalList(List<Guid> guids1, List<Guid> guids2)
+        {
+            List<Guid> totalList = guids1;
+
+            foreach (var guid in guids2)
+            {
+                if (!totalList.Contains(guid))
+                {
+                    totalList.Add(guid);
+                    Console.WriteLine(guid);
+                }
+            }
+
+            return totalList;
+        }
+
+        List<Guid> GetGuids(string str)
+        {
+            List<Guid> guids = new List<Guid>();
+
+            if (str.StartsWith("~"))
+            {
+                var prefAndJson = str.Split('[');
+
+                if (prefAndJson.Length == 2)
+                {
+                    prefix = prefAndJson[0];
+                    str = '[' + prefAndJson[1];
+                }
+                else
+                {
+                    throw new Exception("Некорректные данные");
+                }
+            }
+
+            guids = JsonConvert.DeserializeObject<List<Guid>>(str);
+
+            return guids;
+        }
+
+        #endregion
     }
 }
