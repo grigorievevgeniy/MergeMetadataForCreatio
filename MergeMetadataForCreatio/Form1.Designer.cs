77f78e49500cd81bfcf04162bfc5c6237ac2b856
Grigoriev <timeinvest27@gmail.com>
2021-05-30 23:08:31 +0300

    Добавил оформление текста, копирования в буфер обмена.

--- MergeMetadataForCreatio/Form1.Designer.cs
+++ MergeMetadataForCreatio/Form1.Designer.cs
@@ -29,29 +29,14 @@ namespace MergeMetadataForCreatio
         /// </summary>
         private void InitializeComponent()
         {
-            this.tbValue1 = new System.Windows.Forms.TextBox();
-            this.tbValue2 = new System.Windows.Forms.TextBox();
             this.lblFirstValue = new System.Windows.Forms.Label();
             this.btnMerge = new System.Windows.Forms.Button();
             this.lblSecondValue = new System.Windows.Forms.Label();
             this.tbResult = new System.Windows.Forms.TextBox();
+            this.tbValue1 = new System.Windows.Forms.RichTextBox();
+            this.tbValue2 = new System.Windows.Forms.RichTextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // tbValue1
-            // 
-            this.tbValue1.Location = new System.Drawing.Point(13, 24);
-            this.tbValue1.Multiline = true;
-            this.tbValue1.Name = "tbValue1";
-            this.tbValue1.Size = new System.Drawing.Size(600, 213);
-            this.tbValue1.TabIndex = 0;
-            // 
-            // tbValue2
-            // 
-            this.tbValue2.Location = new System.Drawing.Point(619, 24);
-            this.tbValue2.Multiline = true;
-            this.tbValue2.Name = "tbValue2";
-            this.tbValue2.Size = new System.Drawing.Size(600, 213);
-            this.tbValue2.TabIndex = 1;
             // 
             // lblFirstValue
             // 
@@ -64,8 +49,8 @@ namespace MergeMetadataForCreatio
             // 
             // btnMerge
             // 
-            this.btnMerge.Font = new System.Drawing.Font("Cooper Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMerge.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnMerge.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMerge.ForeColor = System.Drawing.SystemColors.InfoText;
             this.btnMerge.Location = new System.Drawing.Point(576, 243);
             this.btnMerge.Name = "btnMerge";
             this.btnMerge.Size = new System.Drawing.Size(75, 23);
@@ -85,23 +70,55 @@ namespace MergeMetadataForCreatio
             // 
             // tbResult
             // 
+            this.tbResult.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tbResult.Location = new System.Drawing.Point(12, 272);
             this.tbResult.Multiline = true;
             this.tbResult.Name = "tbResult";
             this.tbResult.Size = new System.Drawing.Size(1206, 124);
             this.tbResult.TabIndex = 5;
             // 
+            // tbValue1
+            // 
+            this.tbValue1.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbValue1.Location = new System.Drawing.Point(12, 24);
+            this.tbValue1.Name = "tbValue1";
+            this.tbValue1.Size = new System.Drawing.Size(601, 213);
+            this.tbValue1.TabIndex = 6;
+            this.tbValue1.Text = "";
+            // 
+            // tbValue2
+            // 
+            this.tbValue2.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbValue2.Location = new System.Drawing.Point(619, 24);
+            this.tbValue2.Name = "tbValue2";
+            this.tbValue2.Size = new System.Drawing.Size(601, 213);
+            this.tbValue2.TabIndex = 7;
+            this.tbValue2.Text = "";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCopy.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnCopy.Location = new System.Drawing.Point(555, 402);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(135, 23);
+            this.btnCopy.TabIndex = 8;
+            this.btnCopy.Text = "COPY CLIPBOARD";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1230, 495);
+            this.ClientSize = new System.Drawing.Size(1230, 435);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.tbValue2);
+            this.Controls.Add(this.tbValue1);
             this.Controls.Add(this.tbResult);
             this.Controls.Add(this.lblSecondValue);
             this.Controls.Add(this.btnMerge);
             this.Controls.Add(this.lblFirstValue);
-            this.Controls.Add(this.tbValue2);
-            this.Controls.Add(this.tbValue1);
             this.Name = "Form1";
             this.Text = "Merge Metadata From Creatio";
             this.ResumeLayout(false);
@@ -110,13 +127,13 @@ namespace MergeMetadataForCreatio
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox tbValue1;
-        private System.Windows.Forms.TextBox tbValue2;
         private System.Windows.Forms.Label lblFirstValue;
         private System.Windows.Forms.Button btnMerge;
         private System.Windows.Forms.Label lblSecondValue;
         private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.RichTextBox tbValue1;
+        private System.Windows.Forms.RichTextBox tbValue2;
+        private System.Windows.Forms.Button btnCopy;
     }
 }
 
--- MergeMetadataForCreatio/Form1.cs
+++ MergeMetadataForCreatio/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace MergeMetadataForCreatio
@@ -12,6 +13,11 @@ namespace MergeMetadataForCreatio
         public Form1()
         {
             InitializeComponent();
+
+            //ToDo
+            // Добавить подписи
+            // Текстовое описание
+            // Видео призентацию
         }
 
         private void btnMerge_Click(object sender, EventArgs e)
@@ -29,6 +35,71 @@ namespace MergeMetadataForCreatio
             var finishGuids = GetTotalList(guids1, guids2);
 
             tbResult.Text = ConvertGuidsToString(finishGuids);
+
+            RefreshTextBoxes(guids1, guids2);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e) => Clipboard.SetText(tbResult.Text);
+
+        private void RefreshTextBoxes(List<Guid> guids1, List<Guid> guids2)
+        {
+            tbValue1.Clear();
+            tbValue1.AppendText(prefix + "[\n", Color.Blue);
+            for (int i = 0; i < guids1.Count; i++)
+            {
+                if (guids2.Contains(guids1[i]))
+                {
+                    tbValue1.AppendText("\"" + guids1[i] + "\",", Color.Blue);
+                    if (i == guids1.Count - 1)
+                    {
+                        tbValue1.AppendText(",", Color.Blue);
+                    }
+                }
+                else
+                {
+                    tbValue1.AppendText("\"" + guids1[i] + "\"", Color.Red);
+                    if (i == guids1.Count - 1)
+                    {
+                        tbValue1.AppendText(",", Color.Red);
+                    }
+                }
+
+                if (i % 2 == 1 && i != guids1.Count - 1)
+                {
+                    tbValue1.AppendText("\n");
+                }
+            }
+            tbValue1.AppendText("]", Color.Blue);
+
+            tbValue2.Clear();
+            tbValue2.AppendText(prefix + "[\n", Color.Blue);
+            for (int i = 0; i < guids2.Count; i++)
+            {
+                if (guids1.Contains(guids2[i]))
+                {
+                    tbValue2.AppendText("\"" + guids2[i] + "\",", Color.Blue);
+                    if (i == guids1.Count - 1)
+                    {
+                        tbValue1.AppendText(",", Color.Blue);
+                    }
+                }
+                else
+                {
+                    tbValue2.AppendText("\"" + guids2[i] + "\",", Color.Red);
+                    if (i == guids1.Count - 1)
+                    {
+                        tbValue1.AppendText(",", Color.Red);
+                    }
+                }
+
+                //ToDo удалить последнюю запятую
+
+                if (i % 2 == 1 && i != guids1.Count - 1)
+                {
+                    tbValue2.AppendText("\n");
+                }
+            }
+            tbValue2.AppendText("]", Color.Blue);
         }
 
         #region Help Methods
@@ -37,14 +108,19 @@ namespace MergeMetadataForCreatio
 
         List<Guid> GetTotalList(List<Guid> guids1, List<Guid> guids2)
         {
-            List<Guid> totalList = guids1;
+            List<Guid> totalList = new List<Guid>();
+
+            foreach (var guid in guids1)
+            {
+                totalList.Add(guid);
+            }
+
 
             foreach (var guid in guids2)
             {
                 if (!totalList.Contains(guid))
                 {
                     totalList.Add(guid);
-                    Console.WriteLine(guid);
                 }
             }
 
